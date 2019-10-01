5a4c302ce26a15a03f0189b26b9ecc0e99634977
ahmedng <ahmedng207@gmail.com>
2019-10-01 06:37:38 +0200

    Updates

---
+++ precache-manifest.28ffd4ffa8a55d64a2b7284cd711191f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b74f104ac5457de99a91eaa58d5f97",
+    "url": "/UltraReact/index.html"
+  },
+  {
+    "revision": "7f114552e2d4fa36b4bb",
+    "url": "/UltraReact/static/css/main.bfde34fb.chunk.css"
+  },
+  {
+    "revision": "19b9390dd769d99afde7",
+    "url": "/UltraReact/static/js/2.e31bcfac.chunk.js"
+  },
+  {
+    "revision": "7f114552e2d4fa36b4bb",
+    "url": "/UltraReact/static/js/main.5d130aae.chunk.js"
+  },
+  {
+    "revision": "73a782853c55cad53fdb",
+    "url": "/UltraReact/static/js/runtime-main.68f235cd.js"
+  },
+  {
+    "revision": "020c9eb834982825b30175d37a0d17d0",
+    "url": "/UltraReact/static/media/about-bg.020c9eb8.jpg"
+  },
+  {
+    "revision": "6b1d7b9249e7cdf3d86b567d3032115a",
+    "url": "/UltraReact/static/media/home-bg.6b1d7b92.jpg"
+  },
+  {
+    "revision": "e72c2187023a4f81e5beb99fc01e51f9",
+    "url": "/UltraReact/static/media/portfolio-img1.e72c2187.jpg"
+  },
+  {
+    "revision": "178ffa234e75dfd850b04b650bd773c8",
+    "url": "/UltraReact/static/media/portfolio-img2.178ffa23.jpg"
+  },
+  {
+    "revision": "91f85a2bf75b457555982a3743ca76c3",
+    "url": "/UltraReact/static/media/portfolio-img3.91f85a2b.jpg"
+  },
+  {
+    "revision": "03b826c600c300db9d78b22726166a33",
+    "url": "/UltraReact/static/media/portfolio-img4.03b826c6.jpg"
+  },
+  {
+    "revision": "08c9320c5e5181f30d939a6dc5b7c6d1",
+    "url": "/UltraReact/static/media/portfolio-img5.08c9320c.jpg"
+  },
+  {
+    "revision": "3cd14e36160e75f1e4c2a960bc85423c",
+    "url": "/UltraReact/static/media/portfolio-img6.3cd14e36.jpg"
+  },
+  {
+    "revision": "1751e3ccf93843a345010795fc79dcd4",
+    "url": "/UltraReact/static/media/portfolio-img7.1751e3cc.jpg"
+  },
+  {
+    "revision": "bd46a53097c1c0cc4d916c2a724c6627",
+    "url": "/UltraReact/static/media/portfolio-img8.bd46a530.jpg"
+  }
+]);
